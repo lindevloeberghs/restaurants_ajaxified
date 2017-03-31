a951296bb38fcdbc52d851bcc79db7cdd8371a10
Linde Vloeberghs <lindevloeberghs@me.com>
2017-03-31 11:48:55 +0200

    add ajaxified create action

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,6 +22,8 @@ module RestaurantsAjaxified
     config.generators do |generate|
       generate.assets false
     end
+
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
